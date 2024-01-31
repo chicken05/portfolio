00888e2e3a5888314ddb794b56e17a2ff0bd4945
chicken05 <rickberglund05@gmail.com>
2024-01-30 22:16:58 -0500

    portfolio

---
+++ info.py
@@ -0,0 +1,74 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/pfp.jpg"
+about_me = "My name is Rick Berglund. I am currently a first year computer science student at Tech, interested in the applications of CS in art."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/rick-berglund-b5003b28a"
+my_github_url = "https://github.com/chicken05"
+my_email_address = "rickberglund05@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Spring 2027',
+    'GPA': '4.0/4.0'
+}
+course_data = {
+    "code":["CS 1301", "CS 1331"], 
+    "names":["Introduction to Computing", "Introduction to Object Oriented Programming"], 
+    "semester_taken":["1st", "2nd"],
+    "skills":["How to do recursion and more in Python😎", "How to do Java"],
+    }
+experience_data = {
+    "Bursar's Office: Student Assistant II": (["- Sort confidential documents",
+                                                                          "- Stuff envelopes with mail and checks", "- Help develop digital marketplaces"],"images/money.webp"),
+}
+
+projects_data = {
+    "DHHS IB Website": "Created Druid Hills High School's IB Website, desgning UX elements.",
+    "Food Website": "Used HTML and CSS to create a website about my favorite food.",
+    "Apple Survey": "Made website survey using HTML, CSS, and JavaScript to figure out what kind of apple a person is."
+}
+
+programming_data = {
+    "Python": 60,
+    "Java": 30,
+    "C": 0,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Japanese":"🇯🇵"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Intermediate",
+    "Japanese": "Advanced",
+}
+leadership_data = {
+    "DHHS Environmental Club": (["- President in my senior year of high school", "- Expanded school garden and donated produce to locals."],"images/earth.png"),
+    "DHHS Marching Band": (["- Trumpet Section Leader", "- 6 years playing in the DHHS Marching Band."],"images/trumpet.png"),
+
+}
+activity_data={
+    "Park Adoption": ["- Cleaned local park's waterways/trails.", "- Revitalized local park through mulching."]
+}
